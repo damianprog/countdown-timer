d4205197368230f42e2fa535f66f35521e1c761e
damianprog <damianpukszta@gmail.com>
2019-08-24 18:03:14 +0200

    count down with time

--- resources/js/timer-logic.js
+++ resources/js/timer-logic.js
@@ -1,18 +1,50 @@
-const timeInterval = window.setInterval(setTime,1000);
+function startCountdown() {
+    const dateInput = document.querySelector(".date-input").value;
+    const timeInput = document.querySelector(".time-input").value;
 
-function setTime() {
-    const currentDate = new Date();
+    let currentDate = new Date();
+    let userDate = new Date(dateInput);
+
+    if (timeInput !== "") {
+        const hoursMinutesSeconds = timeInput.split(":");
+        userDate.setHours(
+            parseInt(hoursMinutesSeconds[0]),
+            parseInt(hoursMinutesSeconds[1]),
+            parseInt(hoursMinutesSeconds[2])
+        );
+    }
+
+    if (dateInput) {
+        let timeDiff = getDatesDifference(currentDate,userDate);
+
+        window.setInterval(() => {
+            setTime(timeDiff);
+            timeDiff = timeDiff - 1000;
+        },1000);
+    }
+}
+
+function getDatesDifference(a,b) {
+    let date1 = a.getTime();
+    let date2 = b.getTime();
+
+    return Math.abs(date1 - date2);
+}
+
+function setTime(timeDiff) {
 
     const secondsCount = document.querySelector(".seconds");
     const minutesCount = document.querySelector(".minutes");
     const hoursCount = document.querySelector(".hours");
     const daysCount = document.querySelector(".days");
 
-    secondsCount.innerHTML = currentDate.getSeconds().toString();
-    minutesCount.innerHTML = currentDate.getMinutes().toString();
-    hoursCount.innerHTML = currentDate.getHours().toString();
-    daysCount.innerHTML = currentDate.getDay().toString();
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff - (days * 1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff - ((days * 1000 * 60 * 60 * 24) + (hours * 1000 * 60 * 60))) / (1000 * 60));
+    const seconds = Math.floor((timeDiff - ((days * 1000 * 60 * 60 * 24) + (hours * 1000 * 60 * 60) + (minutes * 1000 * 60))) / 1000);
 
-
-
+    daysCount.innerHTML = days.toString();
+    hoursCount.innerHTML = hours.toString();
+    minutesCount.innerHTML = minutes.toString();
+    secondsCount.innerHTML = seconds.toString();
 }
