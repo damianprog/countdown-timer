a5611d9f9cbb9349099c9518157412678797d384
damianprog <damianpukszta@gmail.com>
2019-08-27 00:30:40 +0200

    Updated functions

--- resources/js/timer-logic.js
+++ resources/js/timer-logic.js
@@ -1,50 +1,56 @@
+const inputs = document.querySelector(".inputs");
+const eventInfo = document.querySelector(".event-info");
+const startButton = document.querySelector(".start-button");
+
 function startCountdown() {
     const dateInput = document.querySelector(".date-input").value;
     const timeInput = document.querySelector(".time-input").value;
 
-    let currentDate = new Date();
-    let userDate = new Date(dateInput);
+    if (dateInput) {
+        let userDate = new Date(dateInput);
 
-    if (timeInput !== "") {
-        const hoursMinutesSeconds = timeInput.split(":");
+        const hoursMinutesSeconds = timeInput !== "" ? timeInput.split(":") : ["0", "0", "0"];
         userDate.setHours(
             parseInt(hoursMinutesSeconds[0]),
             parseInt(hoursMinutesSeconds[1]),
-            parseInt(hoursMinutesSeconds[2])
+            hoursMinutesSeconds[2] ? parseInt(hoursMinutesSeconds[2]) : 0
         );
-    }
 
-    if (dateInput) {
-        let timeDiff = getDatesDifference(currentDate,userDate);
+        showEventLabels();
 
         window.setInterval(() => {
-            setTime(timeDiff);
-            timeDiff = timeDiff - 1000;
-        },1000);
+            setTime(getTimeDifference(userDate));
+        }, 1000);
     }
 }
 
-function getDatesDifference(a,b) {
-    let date1 = a.getTime();
-    let date2 = b.getTime();
+function showEventLabels() {
+    const eventInput = document.querySelector(".event-input").value;
+
+    inputs.style.display = "none";
+
+    eventInfo.style.display = "block";
+    eventInfo.innerHTML = `<h1>${eventInput}</h1>`;
+
+    startButton.style.backgroundColor = "#8b1616";
+    startButton.innerHTML = "Stop";
+}
+
+function getTimeDifference(userDate) {
+    let date1 = new Date().getTime();
+    let date2 = userDate.getTime();
 
     return Math.abs(date1 - date2);
 }
 
 function setTime(timeDiff) {
-
-    const secondsCount = document.querySelector(".seconds");
-    const minutesCount = document.querySelector(".minutes");
-    const hoursCount = document.querySelector(".hours");
-    const daysCount = document.querySelector(".days");
-
     const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDiff - (days * 1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDiff - ((days * 1000 * 60 * 60 * 24) + (hours * 1000 * 60 * 60))) / (1000 * 60));
     const seconds = Math.floor((timeDiff - ((days * 1000 * 60 * 60 * 24) + (hours * 1000 * 60 * 60) + (minutes * 1000 * 60))) / 1000);
 
-    daysCount.innerHTML = days.toString();
-    hoursCount.innerHTML = hours.toString();
-    minutesCount.innerHTML = minutes.toString();
-    secondsCount.innerHTML = seconds.toString();
+    document.querySelector(".days").innerHTML = days.toString();
+    document.querySelector(".hours").innerHTML = hours.toString();
+    document.querySelector(".minutes").innerHTML = minutes.toString();
+    document.querySelector(".seconds").innerHTML = seconds.toString();
 }
